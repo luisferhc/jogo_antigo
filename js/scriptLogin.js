3e7ce33760ae5b42d7a70a4355298d88dea90e79
unknown <heringcoelho@gmail.com>
2022-02-17 10:33:57 -0300

    tela de login e layout home

---
+++ js/scriptLogin.js
@@ -0,0 +1,8 @@
+let email = document.getElementById("inEmail");
+let password = document.getElementById("inPassword")
+
+function registrarLogin() {
+    let emailDigitado = email.value;
+    let senhaDigitada = password.value;
+    console.log(emailDigitado, senhaDigitada)
+}
